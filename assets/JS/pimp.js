5b5e13b05e5f70cedf84a8eed9c85c3fb088bd2c
Pat <44550032+ItsPat47@users.noreply.github.com>
2019-03-07 22:05:53 +0100

    first commit

---
+++ assets/JS/pimp.js
@@ -0,0 +1,22 @@
+[].forEach.call(document.querySelectorAll("a"), el => {
+  el.addEventListener("click", () => {
+    // votre code ici...…
+    console.log("AAAAAAh");
+    document.getElementById("menusmart").style.display = "block";
+    document.getElementById("cross").style.display = "block";
+    document.getElementById("supp").style.display = "none";
+  });
+});
+
+var gross = document.getElementById("cross");
+
+gross.onclick = function() {
+  document.getElementById("menusmart").style.display = "none";
+  document.getElementById("cross").style.display = "none";
+  document.getElementById("supp").style.display = "block";
+};
+
+window.addEventListener("scroll", () => {
+  let scroll = window.scrollY; // Axe Y
+  console.log(scroll);
+});
